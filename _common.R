1dd1227cd9cf39681ebf67a6886ae7bb763d871d
bbartholdy <47455812+bbartholdy@users.noreply.github.com>
2023-06-01 14:24:47 +0200

    minor adjustments to bibliography script

--- _common.R
+++ _common.R
@@ -23,24 +23,29 @@ if (!nzchar(Sys.getenv("QUARTO_PROJECT_RENDER_ALL"))) {
 }
 
 library(here)
+library(rbbt)
 
 diss_files <- list.files(here(), pattern = ".Rmd|.qmd", full.names = T)
 byoc_valid_files <- list.files(here("articles/byoc-valid/07-publication"), pattern = ".Rmd|.qmd", full.names = T)
 byoc_starch_files <- list.files(here("articles/byoc-starch/analysis/paper"), pattern = ".Rmd|.qmd", full.names = T)
 mb11_file_files <- list.files(here("articles/mb11CalculusPilot/analysis/paper"), pattern = ".Rmd|.qmd", full.names = T)
-all_file_names <- c(diss_files, byoc_valid_files, byoc_starch_files, mb11_file_files)
-keys <- bbt_detect_citations(all_file_names)
+
+
+file_names <- c(diss_files, byoc_valid_files, byoc_starch_files, mb11_file_files)
+all_files <- all_file_names[stringr::str_detect(all_file_names, "supp-mat|fig_tab", negate = T)]
+keys <- bbt_detect_citations(all_files)
 bbt_ignore <- keys[grepl("fig-|tbl-", keys)]
 
+
 if(file.exists(here("book.bib"))) {
-  bbt_update_bib(here(all_file_names),
+  bbt_update_bib(here(all_files),
     here("book.bib"),
     ignore = bbt_ignore,
     overwrite = T)
 } else {
-  refs <- rbbt::bbt_detect_citations(all_file_names)
+  refs <- rbbt::bbt_detect_citations(all_files)
   bbt_write_bib(here("book.bib"),
-    keys = refs,
+    keys = keys,
     ignore = bbt_ignore,
     overwrite = T,
     translator = "bibtex")
